56cde804d92469b5436863b9d073ef5b1da07e12
clotje <41633130+clotje@users.noreply.github.com>
2022-03-31 11:53:43 +0200

    Create selinux.pp

---
+++ manifests/selinux.pp
@@ -0,0 +1,23 @@
+# @summary LIM RHEL8 SELinux RHEL8 Security
+#
+# Setting SELinux to Permissive mode permanently 
+# Set setenforce to 0 -> no reboot needed ....
+#
+$selinux_config = '/etc/selinux/config'
+
+class security::selinux{
+
+file_line{'Setting SELinux to Permissive mode permanently on the client':
+  onlyif => '/bin/test -e $selinux_config',
+  path   => '$selinux_config',
+  line   => 'SELINUX=permissive',
+  match  => "^SELINUX=.*$",
+  notify => Exec['noreboot'],
+}
+
+exec{'noreboot':
+  path        => "/bin:/sbin:/usr/bin:/usr/sbin:",
+  command     => 'setenforce 0',
+  refreshonly => true,
+}
+}
