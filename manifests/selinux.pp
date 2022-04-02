a86094d668272eb72cc541d8cba5073635afd3cf
clotje <41633130+clotje@users.noreply.github.com>
2022-04-02 16:03:38 +0200

    Update selinux.pp

--- manifests/selinux.pp
+++ manifests/selinux.pp
@@ -2,19 +2,25 @@
 #
 # Setting SELinux to Permissive mode permanently 
 #
+$selinx_config = "/etc/selinux/config"
+$check_config  = file_exists('$selinx_config')
 #
 node default{
 
+if $check_config == 1 {
+
 file_line{'Setting SELinux to Permissive mode permanently':
-  path   => '/etc/selinux/config',
+  path   => '$selinx_config',
   line   => 'SELINUX=permissive',
   match  => "^SELINUX=.*$",
   notify => Exec['noreboot'],
+}
 }
 
 exec{'noreboot':
   path        => "/bin:/sbin:/usr/bin:/usr/sbin:",
   command     => 'setenforce 0',
   refreshonly => true,
+    
 }
 }
