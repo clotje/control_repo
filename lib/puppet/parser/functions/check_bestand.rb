d02502a38f04184cccfe31e09134957afb91e0f4
clotje <41633130+clotje@users.noreply.github.com>
2022-04-02 16:54:00 +0200

    Update check_bestand.rb

--- lib/puppet/parser/functions/check_bestand.rb
+++ lib/puppet/parser/functions/check_bestand.rb
@@ -3,7 +3,7 @@
 require 'puppet'
 
 module Puppet::Parser::Functions
-  newfunction(:file_exists, :type => :rvalue) do |args|
+  newfunction(:check_bestand, :type => :rvalue) do |args|
     if File.exists?(args[0])
       return 1
     else
