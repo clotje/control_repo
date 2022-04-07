8fecfc86ebba764a1b762806334d2d224fae34ba
clotje <41633130+clotje@users.noreply.github.com>
2022-04-07 20:47:25 +0200

    Create selinuxF.pp

---
+++ manifests/selinuxF.pp
@@ -0,0 +1,17 @@
+# @summary LIM RHEL8 SELinux RHEL8 Security
+#
+# Setting SELinux to Enforced mode permanently
+#
+class security::selinux{
+file_line { 'Setting SELinux to "disabled", "permissive", or "enforcing" mode permanently':
+  path   => '/etc/selinux/config',
+  line   => 'SELINUX=enforcing',
+  match  => '^SELINUX=.*$',
+  }
+
+exec { 'noreboot':
+  onlyif      => 'test `getenforce | grep -c Enforcing` -eq 0',
+  path        => '/bin:/sbin:/usr/bin:/usr/sbin:',
+  command     => 'setenforce 1',
+  }
+ }
